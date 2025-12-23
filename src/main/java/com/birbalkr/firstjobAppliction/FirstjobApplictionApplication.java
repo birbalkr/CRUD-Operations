c94c5ea5f4cb296d77615bd29babd770a3f12fba
Black_Louts <wolf@wolf>
2025-12-23 17:41:59 +0530

    setup spring boot ..

---
+++ src/main/java/com/birbalkr/firstjobAppliction/FirstjobApplictionApplication.java
@@ -0,0 +1,13 @@
+package com.birbalkr.firstjobAppliction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjobApplictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjobApplictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birbalkr/firstjobAppliction/FirstjobApplictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birbalkr.firstjobAppliction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjobApplictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
